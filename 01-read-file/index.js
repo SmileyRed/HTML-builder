05f1328809744e3d25cc50216dfcfeee267f4e25
SmileyRed <choperman@yandex.ru>
2023-05-07 14:53:07 +0400

    add: task  "01-read-file" solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+function fileHandler() {
+  const filePath = path.join(__dirname, 'text.txt')
+
+  const stream = fs.createReadStream(filePath, 'utf8');
+  stream.on('data', (data) => {
+    console.log(data)
+  })
+}
+
+fileHandler();
